ead28ab174ba3f26d68d4c086624adb3cae171b8
Fragmare <fragmare@yahoo.com>
2017-02-26 13:23:24 -0600

    Update chipify.bat

--- bin/Release/chipify.bat
+++ bin/Release/chipify.bat
@@ -1,10 +1,20 @@
-rem cleanup from before
+SET /P CHIPRATE=Enter new sample rate in Hz: 
+IF [%CHIPRATE%]==[] GOTO :END
+
+@ECHO Cleanup from before
 del %1.*.dmw
-del %1.small.wav
-del %1.small.wav.txt
+del %1.CHIPIFIED.wav
+del %1.CHIPIFIED.wav.txt
 
-rem decrease volume to a range [16,15] and resample while we're at it
-sox -D -v 0.00048829615 %1 -r 1860 %1.small.wav
+@ECHO Convert to mono, resample and normalize to peak...
+sox -D %1 -c 1 -r "%CHIPRATE%" "%~1.TMP1.wav"
+sox -D "%~1.TMP1.wav" "%~1.TMP2.wav" norm
+@ECHO Readjust volume for 5-bit...
+sox -D -v 0.00048829615 "%~1.TMP2.wav" "%~1.CHIPIFIED.wav"
 
-rem produces myfile.wav.small.wav.0.dmw, etc. with the chip samples you need
-chipifier %1.small.wav
+@ECHO Outputs myfile.wav.CHIPIFIED.wav.0.dmw, etc. wavetable files
+chipifier "%~1.CHIPIFIED.wav"
+
+del %1.TMP*.wav
+
+:END
