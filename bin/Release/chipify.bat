1e8a00baf04df0e928cfefbe1bc7841f9038dd69
Fragmare <fragmare@yahoo.com>
2017-03-07 23:31:52 -0600

    Update chipify.bat

--- bin/Release/chipify.bat
+++ bin/Release/chipify.bat
@@ -1,20 +1,18 @@
 SET /P CHIPRATE=Enter new sample rate in Hz: 
-IF [%CHIPRATE%]==[] GOTO :END
+IF %CHIPRATE% EQU 0 GOTO :END
 
-@ECHO Cleanup from before
+SET /A UserInputVal="%CHIPRATE%"*1
+IF %UserInputVal% EQU 0 GOTO END
+
 del %1.*.dmw
 del %1.CHIPIFIED.wav
 del %1.CHIPIFIED.wav.txt
 
-@ECHO Convert to mono, resample and normalize to peak...
-sox -D %1 -c 1 -r "%CHIPRATE%" "%~1.TMP1.wav"
+sox -D %1 -c 1 -b 16 -r "%CHIPRATE%" "%~1.TMP1.wav"
 sox -D "%~1.TMP1.wav" "%~1.TMP2.wav" norm
-@ECHO Readjust volume for 5-bit...
-sox -D -v 0.00048829615 "%~1.TMP2.wav" "%~1.CHIPIFIED.wav"
+sox -D -v 0.00048828125 "%~1.TMP2.wav" "%~1.CHIPIFIED.wav"
 
-@ECHO Outputs myfile.wav.CHIPIFIED.wav.0.dmw, etc. wavetable files
 chipifier "%~1.CHIPIFIED.wav"
 
-del %1.TMP*.wav
-
 :END
+del %1.TMP*.wav
